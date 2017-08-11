365a8cd53da8961a7b5cdf03d8a292cd73936a7e
rahul <rahulkumar11501@gmail.com>
2017-08-11 10:45:15 +0530

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/'  do
-    # We are going to list our resources here
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
   end
 end
